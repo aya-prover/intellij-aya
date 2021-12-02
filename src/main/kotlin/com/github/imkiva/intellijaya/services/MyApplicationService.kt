66fdb315a2b687750df8df153ff80b6a5ba66874
GitHub Action <action@github.com>
2021-12-02 07:03:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/imkiva/intellijaya/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/imkiva/intellijaya/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.imkiva.intellijaya.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.imkiva.intellijaya.MyBundle
 
 class MyApplicationService {
 
