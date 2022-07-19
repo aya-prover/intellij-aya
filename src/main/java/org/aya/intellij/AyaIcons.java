9f8dd7e54cfc83ae6c669b97a5bc728c824c8a2d
imkiva <imkiva@islovely.icu>
2022-07-19 16:36:47 +0800

    RUN: RunLineMarkerContributor

--- src/main/java/org/aya/intellij/AyaIcons.java
+++ src/main/java/org/aya/intellij/AyaIcons.java
@@ -1,5 +1,6 @@
 package org.aya.intellij;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.util.IconLoader;
 import org.jetbrains.annotations.NotNull;
 
@@ -7,4 +8,5 @@ import javax.swing.*;
 
 public interface AyaIcons {
   @NotNull Icon AYA_FILE = IconLoader.getIcon("/icons/ayaFile.svg", AyaIcons.class);
+  @NotNull Icon GUTTER_RUN = AllIcons.RunConfigurations.TestState.Run;
 }
