c7a879e6a7f22073a5e373f46bdb7983c8e37c86
imkiva <imkiva@islovely.icu>
2022-07-19 16:36:47 +0800

    RUN: RunLineMarkerContributor

---
+++ src/main/java/org/aya/intellij/run/AyaRunLineMarkerContributor.java
@@ -0,0 +1,24 @@
+package org.aya.intellij.run;
+
+import com.intellij.execution.lineMarker.ExecutorAction;
+import com.intellij.execution.lineMarker.RunLineMarkerContributor;
+import com.intellij.psi.PsiElement;
+import kala.collection.immutable.ImmutableSeq;
+import org.aya.intellij.AyaIcons;
+import org.aya.intellij.psi.concrete.AyaPsiDecl;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class AyaRunLineMarkerContributor extends RunLineMarkerContributor {
+  @Override public @Nullable Info getInfo(@NotNull PsiElement psiElement) {
+    if (!(psiElement instanceof AyaPsiDecl)) return null;
+    final var actions = ExecutorAction.getActions(Integer.MAX_VALUE);
+    return new Info(
+      AyaIcons.GUTTER_RUN,
+      actions,
+      element -> ImmutableSeq.of(actions)
+        .mapNotNull(action -> getText(action, element))
+        .joinToString("\n")
+    );
+  }
+}
