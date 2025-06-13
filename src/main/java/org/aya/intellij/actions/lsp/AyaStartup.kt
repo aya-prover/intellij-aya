a4a2de6135123779dcaf99410c252c5a412964db
HoshinoTented <hoshinotented@qq.com>
2025-06-14 00:46:40 +0800

    settings: map todo

--- src/main/java/org/aya/intellij/actions/lsp/AyaStartup.kt
+++ src/main/java/org/aya/intellij/actions/lsp/AyaStartup.kt
@@ -8,31 +8,36 @@ import com.intellij.openapi.startup.ProjectActivity
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import org.aya.intellij.AyaConstants
+import org.aya.intellij.actions.lsp.AyaStartup.Companion.LOGGER
 import org.aya.intellij.externalSystem.settings.AyaSettings
 import org.aya.intellij.service.AyaSettingService
 
 class AyaStartup : ProjectActivity {
   companion object {
-    private val LOGGER = Logger.getInstance(AyaStartup::class.java)
+    val LOGGER = Logger.getInstance(AyaStartup::class.java)
   }
 
   override suspend fun execute(project: Project) {
     val settings = AyaSettingService.getInstance()
     if (!settings.lspEnable()) return
-    val externalSystemSettings = AyaSettings.getInstance(project)
-
-    for (externalProject in externalSystemSettings.linkedProjectsSettings) {
-      val spec = ImportSpecBuilder(project, AyaConstants.SYSTEM_ID)
-        .build()
-
-      val path = externalProject.externalProjectPath
-      LOGGER.info("Automatically refreshing external project: $path")
-      ExternalSystemUtil.refreshProject(path, spec)
-    }
+    refreshAllAyaProjects(project)
   }
 }
 
 private val initLock: Mutex = Mutex()
+
+fun refreshAllAyaProjects(project: Project) {
+  val externalSystemSettings = AyaSettings.getInstance(project)
+
+  for (externalProject in externalSystemSettings.linkedProjectsSettings) {
+    val spec = ImportSpecBuilder(project, AyaConstants.SYSTEM_ID)
+      .build()
+
+    val path = externalProject.externalProjectPath
+    LOGGER.info("Automatically refreshing external project: $path")
+    ExternalSystemUtil.refreshProject(path, spec)
+  }
+}
 
 suspend fun startLsp(project: Project) {
   initLock.withLock {
